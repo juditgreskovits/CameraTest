fc47f9b7979ab5cbfa06b5f12748c837dab08ef5
Judit Greskovits <juditgreskovits@gmail.com>
2014-09-15 17:57:45 +0100

    added hello

--- CameraTest/ViewController.swift
+++ CameraTest/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        println("Hello")
         captureSession.sessionPreset = AVCaptureSessionPresetHigh
         
         let devices = AVCaptureDevice.devices()
